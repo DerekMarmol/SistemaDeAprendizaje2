f6582bbed7891736890e8e0a64b2a019fd054418
Daniel Alexander Ortiz Cabrera <dortizc13@miumg.edu.gt>
2024-05-31 23:46:20 -0600

    Form Centro de Notificaciones

---
+++ FormCentroDeNotificaciones.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace _10Notificaciones
+{
+    partial class CentroDeNotificaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCentroDeNotificaciones));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtEmailFrom = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnFiles = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblFiles = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.txtMessage);
+            this.panel1.Controls.Add(this.txtSubject);
+            this.panel1.Controls.Add(this.txtEmailFrom);
+            this.panel1.Controls.Add(this.btnSend);
+            this.panel1.Controls.Add(this.btnFiles);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.lblFiles);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 11;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(237, 180);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(342, 73);
+            this.txtMessage.TabIndex = 19;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(237, 119);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(342, 20);
+            this.txtSubject.TabIndex = 18;
+            // 
+            // txtEmailFrom
+            // 
+            this.txtEmailFrom.Location = new System.Drawing.Point(237, 64);
+            this.txtEmailFrom.Name = "txtEmailFrom";
+            this.txtEmailFrom.Size = new System.Drawing.Size(342, 20);
+            this.txtEmailFrom.TabIndex = 17;
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnSend.FlatAppearance.BorderSize = 0;
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSend.Location = new System.Drawing.Point(504, 353);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 16;
+            this.btnSend.Text = "Enviar";
+            this.btnSend.UseVisualStyleBackColor = false;
+            // 
+            // btnFiles
+            // 
+            this.btnFiles.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnFiles.FlatAppearance.BorderSize = 0;
+            this.btnFiles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFiles.Location = new System.Drawing.Point(237, 353);
+            this.btnFiles.Name = "btnFiles";
+            this.btnFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnFiles.TabIndex = 15;
+            this.btnFiles.Text = "adjuntos";
+            this.btnFiles.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label5.Location = new System.Drawing.Point(234, 287);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "...";
+            // 
+            // lblFiles
+            // 
+            this.lblFiles.AutoSize = true;
+            this.lblFiles.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblFiles.Location = new System.Drawing.Point(234, 256);
+            this.lblFiles.Name = "lblFiles";
+            this.lblFiles.Size = new System.Drawing.Size(48, 13);
+            this.lblFiles.TabIndex = 13;
+            this.lblFiles.Text = "Adjuntos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Location = new System.Drawing.Point(165, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Mensaje:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Location = new System.Drawing.Point(165, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Asunto:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Location = new System.Drawing.Point(165, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Enviar:";
+            // 
+            // FormCentroDeNotificaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormCentroDeNotificaciones";
+            this.Text = "FormCentroDeNotificaciones";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtEmailFrom;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnFiles;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblFiles;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ FormCentroDeNotificaciones.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10Notificaciones
+{
+    public partial class CentroDeNotificaciones : Form
+    {
+        private string myEmail = "proyecto.Progra00@gmail.com";
+        private readonly string myPassword = "ruoovbweuxihsifh";
+        private string myAlias = "Proyecto Daniel, Derek y Andreina";
+        private string[] myAdjuntos;
+        private MailMessage mCorreo;
+
+        public CentroDeNotificaciones()
+        {
+            InitializeComponent();
+        }
+        private void CrearCuerpoCorreo()
+        {
+            mCorreo = new MailMessage();
+            mCorreo.From = new MailAddress(myEmail, myAlias, System.Text.Encoding.UTF8);
+            mCorreo.To.Add(txtEmailFrom.Text.Trim());
+            mCorreo.Subject = txtSubject.Text.Trim();
+            mCorreo.Body = txtMessage.Text.Trim();
+            mCorreo.IsBodyHtml = true;
+            mCorreo.Priority = MailPriority.High;
+
+            for (int i = 0; i < myAdjuntos.Length; i++)
+            {
+                mCorreo.Attachments.Add(new Attachment(myAdjuntos[i]));
+            }
+        }
+        private void Enviar()
+        {
+            try
+            {
+                SmtpClient smpt = new SmtpClient();
+                smpt.UseDefaultCredentials = false;
+                smpt.Port = 587;
+                smpt.Host = "smtp.gmail.com";
+                smpt.Credentials = new System.Net.NetworkCredential(myEmail, myPassword);
+                ServicePointManager.ServerCertificateValidationCallback = delegate (object s,
+                    X509Certificate certificate, X509Chain chain, SslPolicyErrors erros)
+                { return true; };
+                smpt.EnableSsl = true;
+                smpt.Send(mCorreo);
+                MessageBox.Show("Enviado");
+                lblFiles.Text = "";
+
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message);
+            }
+
+        }
+        private void AdjuntarArchivos()
+        {
+            var names = "";
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.Title = "Adjutar Archivos al Correo";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                myAdjuntos = ofd.FileNames;
+            }
+
+            for (int i = 0; i < myAdjuntos.Length; i++)
+            {
+                names += myAdjuntos[i] + "\n";
+            }
+            lblFiles.Text = names;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFiles_Click_1(object sender, EventArgs e)
+        {
+            AdjuntarArchivos();
+        }
+
+        private void btnSend_Click_1(object sender, EventArgs e)
+        {
+            CrearCuerpoCorreo();
+            Enviar();
+        }
+
+        private void txtEmailFrom_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
