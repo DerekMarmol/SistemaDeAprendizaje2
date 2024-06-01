cf8c4dd01ae01b81b53b8503f5cc22c7db862a0c
DerekMarmol <dmarmols@miumg.edu.gt>
2024-05-31 23:22:29 -0600

    Manejo De Errores

---
+++ FormCatalogoCursos.cs
@@ -0,0 +1,293 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeAprendizaje2
+{
+    public partial class FormCatalogoCursos : Form
+    {
+        string connectionString = "Server=bofn3obbnejxfyoheir1-mysql.services.clever-cloud.com;Database=bofn3obbnejxfyoheir1;User=uh4dunztmvwgo47z;Password=uyjiJZkG5JqLtaELmvku;Port=3306;SslMode=Preferred;";
+        private bool esAdmin;
+        private int usuarioID;
+
+        public string nombre { get; private set; }
+        public string apellido { get; private set; }
+        public string correo { get; private set; }
+
+        public FormCatalogoCursos(bool esAdmin, int usuarioID)
+        {
+            InitializeComponent();
+            this.esAdmin = esAdmin;
+            this.usuarioID = usuarioID;
+
+            btnAgregarCurso.Visible = esAdmin;
+            btnEditarCurso.Visible = esAdmin;
+            btnEliminarCurso.Visible = esAdmin;
+            btnAdministrarMateriales.Visible = esAdmin;
+        }
+
+        private void FormCatalogoCursos_Load(object sender, EventArgs e)
+        {
+            CargarCursosEnDataGridView();
+        }
+
+        private void CargarCursosEnDataGridView()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT CursoID, NombreCurso, Descripcion, Duracion, Instructor, FechaInicio, FechaFin FROM Cursos";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void btnAgregarCurso_Click(object sender, EventArgs e)
+        {
+            FormAgregarCurso formAgregarCurso = new FormAgregarCurso();
+            if (formAgregarCurso.ShowDialog() == DialogResult.OK)
+            {
+                CargarCursosEnDataGridView();
+
+                List<string> correos = ObtenerCorreosUsuarios();
+
+                foreach (var correo in correos)
+                {
+                    EmailHelper.EnviarCorreo(correo, "Nuevo Curso Agregado", "Se ha agregado un nuevo curso: " + formAgregarCurso.NombreCurso);
+                }
+
+            }
+        }
+
+        private List<string> ObtenerCorreosUsuarios()
+        {
+            List<string> correos = new List<string>();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT Email FROM Usuarios";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            correos.Add(reader.GetString("Email"));
+                        }
+                    }
+                }
+            }
+
+            return correos;
+        }
+
+        private void btnEliminarCurso_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                int cursoID = Convert.ToInt32(dataGridView1.CurrentRow.Cells["CursoID"].Value);
+
+                if (MessageBox.Show("¿Estás seguro de que deseas eliminar este curso?", "Confirmar eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    using (MySqlConnection connection = new MySqlConnection(connectionString))
+                    {
+                        try
+                        {
+                            connection.Open();
+
+                            string deleteInscripcionesQuery = "DELETE FROM Inscripciones WHERE CursoID = @CursoID";
+                            MySqlCommand deleteInscripcionesCommand = new MySqlCommand(deleteInscripcionesQuery, connection);
+                            deleteInscripcionesCommand.Parameters.AddWithValue("@CursoID", cursoID);
+                            deleteInscripcionesCommand.ExecuteNonQuery();
+
+                            string deleteCursoQuery = "DELETE FROM Cursos WHERE CursoID = @CursoID";
+                            MySqlCommand deleteCursoCommand = new MySqlCommand(deleteCursoQuery, connection);
+                            deleteCursoCommand.Parameters.AddWithValue("@CursoID", cursoID);
+                            deleteCursoCommand.ExecuteNonQuery();
+
+                            MessageBox.Show("Curso eliminado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error al eliminar el curso: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+
+                    CargarCursosEnDataGridView();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona un curso para eliminar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnEditarCurso_Click(object sender, EventArgs e)
+        {
+            int cursoID = Convert.ToInt32(dataGridView1.CurrentRow.Cells["CursoID"].Value);
+
+            FormEditarCurso formEditarCurso = new FormEditarCurso(cursoID, connectionString);
+            formEditarCurso.ShowDialog();
+
+            CargarCursosEnDataGridView();
+        }
+
+        private void btnRegistrarCurso_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                int cursoID = Convert.ToInt32(dataGridView1.CurrentRow.Cells["CursoID"].Value);
+                int usuarioID = this.usuarioID;
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    string checkInscripcionQuery = "SELECT COUNT(*) FROM Inscripciones WHERE UsuarioID = @UsuarioID AND CursoID = @CursoID";
+                    using (MySqlCommand checkInscripcionCommand = new MySqlCommand(checkInscripcionQuery, connection))
+                    {
+                        checkInscripcionCommand.Parameters.AddWithValue("@UsuarioID", usuarioID);
+                        checkInscripcionCommand.Parameters.AddWithValue("@CursoID", cursoID);
+
+                        connection.Open();
+                        int inscripcionExistente = Convert.ToInt32(checkInscripcionCommand.ExecuteScalar());
+
+                        if (inscripcionExistente > 0)
+                        {
+                            MessageBox.Show("Ya estás inscrito en este curso.");
+                            return;
+                        }
+                    }
+
+                    string query = "INSERT INTO Inscripciones (UsuarioID, CursoID) VALUES (@UsuarioID, @CursoID)";
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@UsuarioID", usuarioID);
+                        command.Parameters.AddWithValue("@CursoID", cursoID);
+
+                        try
+                        {
+                            command.ExecuteNonQuery();
+                            MessageBox.Show("Inscripción exitosa al curso.");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error al inscribirse al curso: " + ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    string query = "SELECT Nombre, Apellido, Email FROM Usuarios WHERE UsuarioID = @UsuarioID";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@UsuarioID", usuarioID);
+
+                        connection.Open();
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                string nombre = reader.GetString("Nombre");
+                                string apellido = reader.GetString("Apellido");
+                                string correo = reader.GetString("Email");
+
+                                this.Hide();
+                                FormInicio formInicio = new FormInicio(esAdmin, usuarioID, nombre, apellido, correo);
+                                formInicio.Show();
+                            }
+                            else
+                            {
+                                MessageBox.Show("No se encontraron datos del usuario.");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void btnVerCursosRegistrados_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Hide();
+                FormCursosRegistrados formCursosRegistrados = new FormCursosRegistrados(usuarioID);
+                formCursosRegistrados.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void btnAdministrarMateriales_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                int cursoID = Convert.ToInt32(dataGridView1.CurrentRow.Cells["CursoID"].Value);
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    string query = "SELECT Nombre, Apellido, Email FROM Usuarios WHERE UsuarioID = @UsuarioID";
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@UsuarioID", usuarioID);
+
+                        connection.Open();
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                this.Hide();
+                                string nombre = reader.GetString("Nombre");
+                                string apellido = reader.GetString("Apellido");
+                                string correo = reader.GetString("Email");
+
+                                FormAdministrarMateriales formAdministrarMateriales = new FormAdministrarMateriales(cursoID, esAdmin, usuarioID, nombre, apellido, correo);
+                                formAdministrarMateriales.ShowDialog();
+                            }
+                            else
+                            {
+                                MessageBox.Show("No se encontraron datos del usuario.");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnEvaluacionesYPruebas_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int cursoID = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["CursoID"].Value);
+
+                EvaluacionYPruebas formEvaluacionYPruebas = new EvaluacionYPruebas(nombre, apellido, correo, cursoID, esAdmin, usuarioID);
+                formEvaluacionYPruebas.Show();
+            }
+            else
+            {
+                MessageBox.Show("Por favor, seleccione un curso.");
+            }
+        }
+    }
+}
---
+++ FormEditarPerfil.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace SistemaDeAprendizaje2
+{
+    partial class FormEditarPerfil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(372, 274);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(112, 23);
+            this.btnConfirmar.TabIndex = 9;
+            this.btnConfirmar.Text = "Confirmar Cambios";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Escribir el apellido nuevo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Escribir el nombre nuevo";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(349, 190);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(150, 20);
+            this.txtApellido.TabIndex = 6;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(349, 141);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(150, 20);
+            this.txtNombre.TabIndex = 5;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // FormEditarPerfil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "FormEditarPerfil";
+            this.Text = "FormEditarPerfil";
+            this.Load += new System.EventHandler(this.FormEditarPerfil_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+    }
+}
