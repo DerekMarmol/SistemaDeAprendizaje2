c558b5911c1a8af86b06964dd634a71ad8f917f9
Daniel Alexander Ortiz Cabrera <dortizc13@miumg.edu.gt>
2024-05-31 23:49:39 -0600

    Form Prueba Y Evaluaciones

---
+++ FormPruebasYEvaluaciones.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace SistemaDeAprendizaje
+{
+    partial class EvaluacionYPruebas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.btnTerminar = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pnlAdmin = new System.Windows.Forms.Panel();
+            this.btnAgregarPregunta = new System.Windows.Forms.Button();
+            this.txtNuevaRespuesta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNuevaPregunta = new System.Windows.Forms.Label();
+            this.txtNuevaPregunta = new System.Windows.Forms.TextBox();
+            this.lblPregunta = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtRespuesta = new System.Windows.Forms.TextBox();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.pnlAdmin.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(433, 295);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(0, 13);
+            this.lblEstado.TabIndex = 21;
+            // 
+            // btnTerminar
+            // 
+            this.btnTerminar.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnTerminar.FlatAppearance.BorderColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnTerminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTerminar.Location = new System.Drawing.Point(376, 317);
+            this.btnTerminar.Margin = new System.Windows.Forms.Padding(0);
+            this.btnTerminar.Name = "btnTerminar";
+            this.btnTerminar.Size = new System.Drawing.Size(130, 23);
+            this.btnTerminar.TabIndex = 20;
+            this.btnTerminar.Text = "Terminar Evaluación";
+            this.btnTerminar.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(31, 198);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 245);
+            this.panel3.TabIndex = 19;
+            // 
+            // pnlAdmin
+            // 
+            this.pnlAdmin.Controls.Add(this.btnAgregarPregunta);
+            this.pnlAdmin.Controls.Add(this.txtNuevaRespuesta);
+            this.pnlAdmin.Controls.Add(this.label1);
+            this.pnlAdmin.Controls.Add(this.lblNuevaPregunta);
+            this.pnlAdmin.Controls.Add(this.txtNuevaPregunta);
+            this.pnlAdmin.Location = new System.Drawing.Point(127, 8);
+            this.pnlAdmin.Name = "pnlAdmin";
+            this.pnlAdmin.Size = new System.Drawing.Size(643, 103);
+            this.pnlAdmin.TabIndex = 18;
+            // 
+            // btnAgregarPregunta
+            // 
+            this.btnAgregarPregunta.Location = new System.Drawing.Point(258, 77);
+            this.btnAgregarPregunta.Name = "btnAgregarPregunta";
+            this.btnAgregarPregunta.Size = new System.Drawing.Size(100, 23);
+            this.btnAgregarPregunta.TabIndex = 4;
+            this.btnAgregarPregunta.Text = "Agregar Pregunta";
+            this.btnAgregarPregunta.UseVisualStyleBackColor = true;
+            // 
+            // txtNuevaRespuesta
+            // 
+            this.txtNuevaRespuesta.Location = new System.Drawing.Point(3, 56);
+            this.txtNuevaRespuesta.Name = "txtNuevaRespuesta";
+            this.txtNuevaRespuesta.Size = new System.Drawing.Size(635, 20);
+            this.txtNuevaRespuesta.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Respuesta Correcta";
+            // 
+            // lblNuevaPregunta
+            // 
+            this.lblNuevaPregunta.AutoSize = true;
+            this.lblNuevaPregunta.Location = new System.Drawing.Point(270, 2);
+            this.lblNuevaPregunta.Name = "lblNuevaPregunta";
+            this.lblNuevaPregunta.Size = new System.Drawing.Size(85, 13);
+            this.lblNuevaPregunta.TabIndex = 1;
+            this.lblNuevaPregunta.Text = "Nueva Pregunta";
+            // 
+            // txtNuevaPregunta
+            // 
+            this.txtNuevaPregunta.Location = new System.Drawing.Point(3, 18);
+            this.txtNuevaPregunta.Name = "txtNuevaPregunta";
+            this.txtNuevaPregunta.Size = new System.Drawing.Size(635, 20);
+            this.txtNuevaPregunta.TabIndex = 0;
+            // 
+            // lblPregunta
+            // 
+            this.lblPregunta.Location = new System.Drawing.Point(373, 114);
+            this.lblPregunta.Name = "lblPregunta";
+            this.lblPregunta.Size = new System.Drawing.Size(125, 24);
+            this.lblPregunta.TabIndex = 17;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblResultado.Location = new System.Drawing.Point(39, 125);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 16;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.btnEnviar);
+            this.panel1.Controls.Add(this.btnSiguiente);
+            this.panel1.Controls.Add(this.txtRespuesta);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 22;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(406, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(349, 321);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Terminar Evaluación";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(4, 202);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 245);
+            this.panel2.TabIndex = 12;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Controls.Add(this.textBox1);
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Controls.Add(this.textBox2);
+            this.panel4.Location = new System.Drawing.Point(100, 12);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(643, 103);
+            this.panel4.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(258, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Agregar Pregunta";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(635, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(255, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Respuesta Correcta";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(270, 2);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Nueva Pregunta";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 18);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(635, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(346, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 24);
+            this.label5.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label6.Location = new System.Drawing.Point(12, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Resultado";
+            // 
+            // txtRespuesta
+            // 
+            this.txtRespuesta.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtRespuesta.Location = new System.Drawing.Point(180, 145);
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(461, 20);
+            this.txtRespuesta.TabIndex = 16;
+            this.txtRespuesta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnSiguiente.FlatAppearance.BorderSize = 0;
+            this.btnSiguiente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSiguiente.Location = new System.Drawing.Point(358, 273);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(113, 23);
+            this.btnSiguiente.TabIndex = 17;
+            this.btnSiguiente.Text = "Siguiente Pregunta";
+            this.btnSiguiente.UseVisualStyleBackColor = false;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnEnviar.FlatAppearance.BorderSize = 0;
+            this.btnEnviar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnviar.Location = new System.Drawing.Point(358, 224);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(113, 23);
+            this.btnEnviar.TabIndex = 18;
+            this.btnEnviar.Text = "Enviar Respuesta";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            // 
+            // FormPruebasYEvaluaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.btnTerminar);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.pnlAdmin);
+            this.Controls.Add(this.lblPregunta);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "FormPruebasYEvaluaciones";
+            this.Text = "FormPruebasYEvaluaciones";
+            this.pnlAdmin.ResumeLayout(false);
+            this.pnlAdmin.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Button btnTerminar;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel pnlAdmin;
+        private System.Windows.Forms.Button btnAgregarPregunta;
+        private System.Windows.Forms.TextBox txtNuevaRespuesta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNuevaPregunta;
+        private System.Windows.Forms.TextBox txtNuevaPregunta;
+        private System.Windows.Forms.Label lblPregunta;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.TextBox txtRespuesta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
