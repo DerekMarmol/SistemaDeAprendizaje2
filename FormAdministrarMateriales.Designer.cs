a1c4218fcf2c661b3b3c19d74fb6fc6c31f18c1c
Daniel Alexander Ortiz Cabrera <dortizc13@miumg.edu.gt>
2024-05-31 23:42:10 -0600

    Form Administar Materiales

---
+++ FormAdministrarMateriales.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace SistemaDeAprendizaje
+{
+    partial class FormAdministrarMateriales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnEliminarMaterial = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblRutaArchivo = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.cboTipoArchivo = new System.Windows.Forms.ComboBox();
+            this.btnSeleccionarArchivo = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(256, 39);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(97, 13);
+            this.lblStatus.TabIndex = 19;
+            this.lblStatus.Text = "Espere, por favor...";
+            this.lblStatus.Visible = false;
+            // 
+            // btnEliminarMaterial
+            // 
+            this.btnEliminarMaterial.Location = new System.Drawing.Point(670, 319);
+            this.btnEliminarMaterial.Name = "btnEliminarMaterial";
+            this.btnEliminarMaterial.Size = new System.Drawing.Size(121, 23);
+            this.btnEliminarMaterial.TabIndex = 18;
+            this.btnEliminarMaterial.Text = "Eliminar Material";
+            this.btnEliminarMaterial.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(10, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(153, 372);
+            this.panel1.TabIndex = 17;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Catalogo De Cursos";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Perfil De Usuario";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lblRutaArchivo
+            // 
+            this.lblRutaArchivo.AutoSize = true;
+            this.lblRutaArchivo.Location = new System.Drawing.Point(610, 101);
+            this.lblRutaArchivo.Name = "lblRutaArchivo";
+            this.lblRutaArchivo.Size = new System.Drawing.Size(0, 13);
+            this.lblRutaArchivo.TabIndex = 16;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(670, 273);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(121, 20);
+            this.txtNombre.TabIndex = 15;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(670, 225);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(121, 20);
+            this.txtDescripcion.TabIndex = 14;
+            // 
+            // cboTipoArchivo
+            // 
+            this.cboTipoArchivo.FormattingEnabled = true;
+            this.cboTipoArchivo.Location = new System.Drawing.Point(670, 177);
+            this.cboTipoArchivo.Name = "cboTipoArchivo";
+            this.cboTipoArchivo.Size = new System.Drawing.Size(121, 21);
+            this.cboTipoArchivo.TabIndex = 13;
+            // 
+            // btnSeleccionarArchivo
+            // 
+            this.btnSeleccionarArchivo.Location = new System.Drawing.Point(670, 133);
+            this.btnSeleccionarArchivo.Name = "btnSeleccionarArchivo";
+            this.btnSeleccionarArchivo.Size = new System.Drawing.Size(121, 23);
+            this.btnSeleccionarArchivo.TabIndex = 12;
+            this.btnSeleccionarArchivo.Text = "Seleccionar Archivo";
+            this.btnSeleccionarArchivo.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(670, 81);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(121, 23);
+            this.btnAgregar.TabIndex = 11;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(169, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(425, 341);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // FormAdministrarMateriales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Peru;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnEliminarMaterial);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblRutaArchivo);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.cboTipoArchivo);
+            this.Controls.Add(this.btnSeleccionarArchivo);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FormAdministrarMateriales";
+            this.Text = "FormAdministrarMateriales";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnEliminarMaterial;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblRutaArchivo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.ComboBox cboTipoArchivo;
+        private System.Windows.Forms.Button btnSeleccionarArchivo;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ FormAdministrarMateriales.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using SistemaDeAprendizaje2;
+
+namespace SistemaDeAprendizaje
+{
+    public partial class FormAdministrarMateriales : Form
+    {
+        private int cursoID;
+        private string connectionString = "Server=bofn3obbnejxfyoheir1-mysql.services.clever-cloud.com;Database=bofn3obbnejxfyoheir1;User=uh4dunztmvwgo47z;Password=uyjiJZkG5JqLtaELmvku;Port=3306;SslMode=Preferred;";
+        private bool esAdmin;
+        private int usuarioID;
+        private string nombre;
+        private string apellido;
+        private string correo;
+        private object nombreArchivo;
+
+        public FormAdministrarMateriales(int cursoID, bool esAdmin, int usuarioID, string nombre, string apellido, string correo)
+        {
+            InitializeComponent();
+            this.cursoID = cursoID;
+            this.esAdmin = esAdmin;
+            this.usuarioID = usuarioID;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.correo = correo;
+            CargarMateriales();
+            LlenarComboBoxTipoArchivo();
+        }
+
+        private void LlenarComboBoxTipoArchivo()
+        {
+            cboTipoArchivo.Items.Add("Documento");
+            cboTipoArchivo.Items.Add("Presentación");
+            cboTipoArchivo.Items.Add("Video");
+            cboTipoArchivo.Items.Add("Otro");
+
+            cboTipoArchivo.SelectedIndex = 0;
+        }
+
+        private void CargarMateriales()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT MaterialID, Nombre, Descripcion, TipoArchivo, RutaArchivo FROM Materiales WHERE CursoID = @CursoID";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+                adapter.SelectCommand.Parameters.AddWithValue("@CursoID", cursoID);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+        }
+
+        private List<string> ObtenerCorreosUsuarios()
+        {
+            List<string> correos = new List<string>();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT Email FROM Usuarios";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            correos.Add(reader.GetString("Email"));
+                        }
+                    }
+                }
+            }
+
+            return correos;
+        }
+
+        private string ObtenerNombreCurso(int cursoID)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT NombreCurso FROM Cursos WHERE CursoID = @CursoID";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@CursoID", cursoID);
+                    connection.Open();
+                    object result = command.ExecuteScalar();
+                    return result != null ? result.ToString() : null;
+                }
+            }
+        }
+
+
+        private async void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string descripcion = txtDescripcion.Text;
+            string tipoArchivo = cboTipoArchivo.SelectedItem.ToString();
+            string rutaArchivo = lblRutaArchivo.Text;
+
+            lblStatus.Visible = true;
+            lblStatus.Text = "Agregando material, por favor espere...";
+
+            await Task.Run(() => AgregarMaterial(nombre, descripcion, tipoArchivo, rutaArchivo));
+
+            lblStatus.Visible = false;
+            CargarMateriales();
+        }
+
+        private void AgregarMaterial(string nombre, string descripcion, string tipoArchivo, string rutaArchivo)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Materiales (CursoID, TipoArchivo, Nombre, RutaArchivo) VALUES (@CursoID, @TipoArchivo, @Nombre, @RutaArchivo)";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@CursoID", cursoID);
+                    command.Parameters.AddWithValue("@TipoArchivo", tipoArchivo);
+                    command.Parameters.AddWithValue("@Nombre", nombre);
+                    command.Parameters.AddWithValue("@RutaArchivo", rutaArchivo);
+
+                    try
+                    {
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Material agregado con éxito.");
+
+                        List<string> correos = ObtenerCorreosUsuarios();
+
+                        foreach (var correo in correos)
+                        {
+                            EmailHelper.EnviarCorreo(correo, "Nuevo Material Agregado", "Se ha agregado un nuevo material para el curso: " + cursoID);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al agregar el material: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+
+        private void btnSeleccionarArchivo_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    lblRutaArchivo.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void FormAdministarMateriales_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EliminarMaterial(int materialID)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Materiales WHERE MaterialID = @MaterialID";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@MaterialID", materialID);
+                    try
+                    {
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Material eliminado con éxito.");
+                        CargarMateriales();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al eliminar el material: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void btnEliminarMaterial_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int materialID = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["MaterialID"].Value);
+
+                if (MessageBox.Show("¿Estás seguro de que deseas eliminar este material?", "Confirmar eliminación", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    EliminarMaterial(materialID);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona un material para eliminar.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    string query = "SELECT Nombre, Apellido, Email FROM Usuarios WHERE UsuarioID = @UsuarioID";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@UsuarioID", usuarioID);
+
+                        connection.Open();
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                string nombre = reader.GetString("Nombre");
+                                string apellido = reader.GetString("Apellido");
+                                string correo = reader.GetString("Email");
+
+                                this.Hide();
+                                FormInicio formInicio = new FormInicio(esAdmin, usuarioID, nombre, apellido, correo);
+                                formInicio.Show();
+                            }
+                            else
+                            {
+                                MessageBox.Show("No se encontraron datos del usuario.");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void cboTipoArchivo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Hide();
+                FormCatalogoCursos formCatalogo = new FormCatalogoCursos(esAdmin, usuarioID);
+                formCatalogo.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+
+}
